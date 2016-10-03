97135dd9387aceeead0888c2bf5ef04f346c1936
Peter Zijlstra <peterz@infradead.org>
2016-10-03 16:35:32 +0200

    UPSTREAM: sched/core: Add missing update_rq_clock() call for task_hot()
    Add the update_rq_clock() call at the top of the callstack instead of
    at the bottom where we find it missing, this to aid later effort to
    minimize the number of update_rq_lock() calls.
    
      WARNING: CPU: 30 PID: 194 at ../kernel/sched/sched.h:797 assert_clock_updated()
      rq->clock_update_flags < RQCF_ACT_SKIP
    
      Call Trace:
        dump_stack()
        __warn()
        warn_slowpath_fmt()
        assert_clock_updated.isra.63.part.64()
        can_migrate_task()
        load_balance()
        pick_next_task_fair()
        __schedule()
        schedule()
        worker_thread()
        kthread()
    
    Change-Id: I17716141789f9fac709495951cd2e079cf49d6d8
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit 3bed5e2166a5e433bf62162f3cd3c5174d335934)
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10518,6 +10518,7 @@ redo:
 
 more_balance:
 		raw_spin_lock_irqsave(&busiest->lock, flags);
+		update_rq_clock(busiest);
 
 		/*
 		 * The world might have changed. Validate assumptions.
@@ -11018,6 +11019,7 @@ static int active_load_balance_cpu_stop(void *data)
 	if (likely(sd)) {
 		env.sd = sd;
 		schedstat_inc(sd->alb_count);
+		update_rq_clock(busiest_rq);
 
 		p = detach_one_task(&env);
 		if (p) {
